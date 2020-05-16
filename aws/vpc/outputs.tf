70d2c0aa5587e86341400de52670943a3628749c
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-05-16 22:33:45 +0400

    Added AWS VPC outputs

---
+++ aws/vpc/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "public_subnet_ids_by_az" {
+  value = module.vpc.public_subnet_ids_by_az
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "private_subnet_ids_by_az" {
+  value = module.vpc.private_subnet_ids_by_az
+}
+
+output "nat_gateway_public_eips" {
+  value = module.vpc.nat_gateway_public_eips
+}
