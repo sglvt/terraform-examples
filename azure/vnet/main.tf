ce17fbda42c8e2ea0f51f59c8e9956ee4bd1ef12
S <S>
2022-07-05 22:42:35 +0400

    azure/vnet - 3 subnets

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -9,9 +9,23 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "${var.stack_name}-snet1"
+resource "azurerm_subnet" "example-a" {
+  name                 = "${var.stack_name}-a"
   virtual_network_name = azurerm_virtual_network.example.name
   resource_group_name  = data.azurerm_resource_group.rm.name
-  address_prefixes     = ["10.0.0.0/20"]
+  address_prefixes     = ["10.0.32.0/20"]
+}
+
+resource "azurerm_subnet" "example-b" {
+  name                 = "${var.stack_name}-b"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.rm.name
+  address_prefixes     = ["10.0.96.0/20"]
+}
+
+resource "azurerm_subnet" "example-c" {
+  name                 = "${var.stack_name}-b"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.rm.name
+  address_prefixes     = ["10.0.160.0/20"]
 }
